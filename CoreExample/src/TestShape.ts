6adf54132e12ad44288099e6de48306b3ca6bc62
WanderWang <wander.wang@ismole.com>
2014-06-13 14:49:10 +0800

    修改 TestShape
    修改 TestShape

--- CoreExample/src/TestShape.ts
+++ CoreExample/src/TestShape.ts
@@ -38,21 +38,22 @@ class TestShape {
 
          var shape1 = new egret.Shape();
          shape1.graphics.beginFill(0xff0000,.5);
-         shape1.graphics.drawRect(200,200,100,100);
+         shape1.graphics.drawRect(50,50,100,100);
          shape1.graphics.endFill();
          container.addChild(shape1);
 
         var shape2 = new egret.Shape();
         shape2.graphics.lineStyle(5,0xffff00,1);
+        shape2.graphics.moveTo(300,0);
         shape2.graphics.lineTo(300,300);
         shape2.graphics.endFill();
         container.addChild(shape2);
         shape2.x = 100;
 
         var shape3 = new egret.Shape();
-        shape1.graphics.beginFill(0xff0000,.5);
-        shape1.graphics.drawCircle(100,100,100);
-        shape1.graphics.endFill();
-        container.addChild(shape1);
+        shape3.graphics.beginFill(0xff0000,.5);
+        shape3.graphics.drawCircle(200,200,100);
+        shape3.graphics.endFill();
+        container.addChild(shape3);
     }
 }
