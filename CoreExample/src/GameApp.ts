b78b97a7850bdbd60aca700742fc4813f4d078a5
dom <dom@domlib.com>
2015-01-07 14:04:22 +0800

    修复TestText例子不显示的问题

--- CoreExample/src/GameApp.ts
+++ CoreExample/src/GameApp.ts
@@ -52,6 +52,7 @@ class GameApp extends egret.DisplayObjectContainer{
 
         //初始化Resource资源加载库，提示：Resource资源加载库是可选模块，不在egret-core项目里，最新代码请到github上的egret-game-library项目检出。
         RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE,this.onResourceLoadComplete,this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR,this.onResourceLoadComplete,this);
         RES.addEventListener(RES.ResourceEvent.GROUP_PROGRESS,this.onResourceProgress,this);
         RES.loadConfig("resource/resource.json","resource/assets/480/");
         RES.loadGroup(this.currentTestName);
