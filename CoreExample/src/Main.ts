febc87d506f3546aa9a33b483ae2dcdadf0e1b4d
zhangyu <runinspring@gmail.com>
2015-09-14 11:41:44 +0800

    init

--- CoreExample/src/Main.ts
+++ CoreExample/src/Main.ts
@@ -1,116 +1,34 @@
-/**
- * Copyright (c) 2014,Egret-Labs.org
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Egret-Labs.org nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY EGRET-LABS.ORG AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL EGRET-LABS.ORG AND CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-2015, Egret Technology Inc.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
 
 class Main extends egret.DisplayObjectContainer {
-
-
     public constructor() {
         super();
-
-        //context
-        ExternalInterfaceExample;
-        localStorageExample;
-        TickerExample;
-
-        //display
-        BitmapExample;
-        BitmapFillModeExample;
-        BlendModeExample;
-        DisplayObjectExample;
-        DisplayObjectContainerExample;
-        GraphicsExample;
-        MovieClipExample;
-        RenderTextureExample;
-        ScrollViewExample;
-        ShapeExample;
-        SpriteExample;
-        SpriteSheetExample;
-        StageExample;
-        StageScaleModeExample;
-        TextureExample;
-
-        //events
-        EventExample;
-        EventDispatcherExample;
-        EventPhaseExample;
-        IEventDispatcherExample;
-        IOErrorEventExample;
-        SoundEventExample;
-        TextEventExample;
-        TimerEventExample;
-        TouchEventExample;
-
-        //geom
-        MatrixExample;
-        PointExample;
-        RectangleExample;
-
-        //media
-        SoundExample;
-        SoundEventExample;
-
-        //net
-        URLLoaderExample;
-        URLLoaderDataFormatExample;
-        URLRequestExample;
-        URLRequestHeaderExample;
-        URLRequestMethodExample;
-        URLVariablesExample;
-
-        //text
-        BitmapFontExample;
-        BitmapTextExample;
-        HtmlTextParserExample;
-        TextFieldExample;
-
-        //tween
-        TweenExample;
-
-        //utils
-        ByteArrayExample;
-        callLaterExample;
-        getDefinitionByNameExample;
-        getQualifiedClassNameExample;
-        getTimerExample;
-        hasDefinitionExample;
-        InjectorExample;
-        RecyclerExample;
-        setIntervalExample;
-        setTimeoutExample;
-        TimerExample;
-        toColorStringExample;
-        XMLExample;
-
-        //socket
-        WebSocketExample;
-        //resource
-        ResourceExample;
-        GetResByUrlExample;
     }
 }
-
-
