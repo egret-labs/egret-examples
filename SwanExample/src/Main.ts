28bf36b87fd9514f2c62d15af8ff9804e2fe0446
zhangyu <runinspring@gmail.com>
2015-09-14 17:04:03 +0800

    update eui

--- SwanExample/src/Main.ts
+++ SwanExample/src/Main.ts
@@ -1,104 +1,62 @@
-﻿class Main extends swan.Group {
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-2015, Egret Technology Inc.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+class Main extends eui.Component {
 
-    green_skins = [
-        "resource/components/green/ListGroup.exml",
-        "resource/components/green/TabBar.exml",
-        "resource/components/MainGroup.exml"
-    ];
-
-    blue_skins = [
-        "resource/components/blue/ListGroup.exml",
-        "resource/components/blue/TabBar.exml",
-        "resource/components/MainGroup.exml"
-    ];
+    btnBlue: eui.Button;
+    btnGreen: eui.Button;
 
     constructor() {
         super();
+        this.skinName = "skins/MainSkin.exml";
+        this.addEventListener(egret.Event.COMPLETE, this.onCompleted, this);
     }
 
-    protected createChildren():void {
-        super.createChildren();
-        this.width = this.stage.stageWidth;
+    protected onCompleted(): void {
+
+        this.btnBlue.addEventListener(egret.TouchEvent.TOUCH_TAP, e=> this.loadTheme("blue"), this);
+        this.btnGreen.addEventListener(egret.TouchEvent.TOUCH_TAP, e=> this.loadTheme("green"), this);
+
+        this.resize();
+        this.stage.addEventListener(egret.Event.RESIZE, this.resize, this);
+    }
+
+    private loadTheme(name: string): void {
+        var theme = new eui.Theme(`resource/theme/${name}-theme.json`, this.stage);
+        //theme.addEventListener(egret.Event.COMPLETE, e=> this.addChild(new components.MainGroup()), this);
+        theme.addEventListener(egret.Event.COMPLETE, ()=>{
+            var group:eui.Group = new components.MainGroup();
+            this.addChild(group);
+        }, this);
+    }
+
+    resize() {
         this.height = this.stage.stageHeight;
-
-        var bg:swan.Image = new swan.Image();
-        bg.source = "resource/assets/blackBg.png";
-        bg.percentHeight = 100;
-        bg.percentWidth = 80;
-        this.addChild(bg);
-
-        var label:swan.Label = new swan.Label();
-        label.horizontalCenter = 0;
-        label.verticalCenter = -40;
-        label.text = "选择一个主题";
-        label.textColor=0xFFFFFF;
-        this.addChild(label);
-
-        var themeGroup:swan.Group = new swan.Group();
-        themeGroup.horizontalCenter = 0;
-        themeGroup.verticalCenter = 0;
-        themeGroup.layout = new swan.HorizontalLayout();
-        themeGroup.touchEnabled = true;
-        themeGroup.addEventListener(egret.TouchEvent.TOUCH_TAP , this.touchTheme , this);
-        this.addChild(themeGroup);
-
-        var image:swan.Image = new swan.Image();
-        //image.touchEnabled = true;
-        image.name = "blue";
-        image.width = 100;
-        image.height = 40;
-        image.source = "resource/assets/blue/Panel/header.png";
-        themeGroup.addChild(image);
-
-        var image2:swan.Image = new swan.Image();
-        //image2.touchEnabled = true;
-        image2.name = "green";
-        image2.width = 100;
-        image2.height = 40;
-        image2.source = "resource/assets/green/Panel/header.png";
-        themeGroup.addChild(image2);
-    }
-
-    private themeName:string;
-    private touchTheme(event:egret.TouchEvent):void
-    {
-        if(event.target.name)
-        {
-            var name = event.target.name;
-            this.themeName = name;
-            var skins = [
-                "resource/components/"+name+"/ListGroup.exml",
-                "resource/components/"+name+"/TabBar.exml",
-                "resource/components/CloseButton.exml",
-                "resource/components/CancelButton.exml"
-            ];
-
-
-            Loader.load(skins, () => {
-                Loader.load(["resource/components/MainGroup.exml"], () => this.loaded());
-            });
-        }
-    }
-
-    loaded() {
-        new swan.Theme("resource/theme/"+this.themeName+"-theme.json",this.stage);
-        this.removeChildren();
-        var ui = utils.createClass("components.MainGroup");
-        this.addChild(ui);
-    }
-}
-
-class Loader {
-    static load(urls: string[], callback: () => void) {
-
-        var total = urls.length;
-        var got = 0;
-        urls.forEach(url => {
-            EXML.load(url,()=>{
-                got++;
-                if (got == total)
-                    callback();
-            },this)
-        });
+        this.width = this.stage.stageWidth;
     }
 }
---
+++ SwanExample/src/components/MainGroup.d.ts
@@ -0,0 +1,3 @@
+declare module components {
+    export class MainGroup extends eui.Group { }
+}
