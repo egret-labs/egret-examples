ce0950da60156b8ce77ad78943073ba06d5a3a42
yjtx <friendhhy@163.com>
2015-09-15 18:09:30 +0800

    compatible

---
+++ examples.js
@@ -0,0 +1,104 @@
+var list = [
+
+    //external
+    "ExternalInterface",
+    //localStorage
+    "localStorage",
+    //display
+    "Bitmap",
+    "BitmapFillMode",
+    "BlendMode",
+    "DisplayObject",
+    "DisplayObjectContainer",
+    "Graphics",
+    "RenderTexture",
+    "Shape",
+    "Sprite",
+    "SpriteSheet",
+    "Stage",
+    "Texture",
+    //player
+    "StageScaleMode",
+    //events
+    "Event",
+    "EventDispatcher",
+    "EventPhase",
+    "IEventDispatcher",
+    "IOErrorEvent",
+    "TextEvent",
+    "TimerEvent",
+    //geom
+    "Matrix",
+    "Point",
+    "Rectangle",
+    //media
+    "Sound",
+    "Video",
+    //text
+    "BitmapFont",
+    "BitmapText",
+    "HtmlTextParser",
+    "TextField",
+
+    //utils
+    "ByteArray",
+    "callLater",
+    "getDefinitionByName",
+    "getOptionName",
+    "getQualifiedClassName",
+    "getQualifiedSuperClassName",
+    "getTimer",
+    "hasDefinition",
+    "is",
+    "numberUtils",
+    "Timer",
+    "toColorString",
+    "XML",
+    //extension
+    //Dragonbones
+    "Dragonbones",
+    //game-display
+    "MovieClip",
+    "ScrollView",
+    //game-net
+    "URLLoader",
+    "URLLoaderDataFormat",
+    "URLRequest",
+    "URLRequestHeader",
+    "URLRequestMethod",
+    "URLVariables",
+    //game-player
+    "Ticker",
+    //game-tuils
+    "Recycler",
+    "setInterval",
+    "setTimeout",
+    //resource
+    "Resource",
+    "GetResByUrl",
+    //socket
+    "WebSocket",
+    //tween
+    "Tween"
+];
+
+var examples = {
+    "Core": list,
+    "EUI": ["all"],
+    "GUI": ["ocean", "simple"]
+};
+
+
+function returnHref(thirdName, forthName) {
+    if (thirdName.indexOf("GUI") >= 0) {
+        return "GUIExample/bin-debug/index.html?mainClass=" + forthName + "&test=" + forthName + "&r=" + Math.random();
+    }
+    else if (thirdName.indexOf("EUI") >= 0) {
+        return "EUIExample/bin-debug/index.html?mainClass=" + (forthName) + "&test=" + forthName + "&r=" + Math.random();
+    }
+    else {
+        return "CoreExample/bin-debug/index.html?mainClass=" + (forthName + "Example") + "&test=" + forthName + "&r=" + Math.random();
+    }
+}
+
+createRoot("Egret", "examples", examples, returnHref);
