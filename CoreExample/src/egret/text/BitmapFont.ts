8c5a034c62978aceca7f751c73272ce37b3eecdf
zhangyu <runinspring@gmail.com>
2015-12-10 14:20:41 +0800

    update

--- CoreExample/src/egret/text/BitmapFont.ts
+++ CoreExample/src/egret/text/BitmapFont.ts
@@ -5,7 +5,8 @@ class BitmapFontExample extends egret.DisplayObjectContainer {
     public constructor() {
         super();
 
-
+        RES.getResByUrl("resource/assets/font.fnt", this.onLoadComplete, this,
+            RES.ResourceItem.TYPE_FONT);
     }
 
     private onLoadComplete(font:egret.BitmapFont):void {
