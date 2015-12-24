57b8a1f1579d8c24f7a21cf721b67c492358f970
zhangyu <runinspring@gmail.com>
2015-12-24 16:00:50 +0800

    update

--- CoreExample/src/extension/eui/components/TextInputExample.ts
+++ CoreExample/src/extension/eui/components/TextInputExample.ts
@@ -11,7 +11,7 @@ class TextInputExample extends eui.Group{
         super();
         var textInputSkin = `
             <e:Skin class="skins.TextInputSkin" minHeight="40" minWidth="300" states="normal,disabled,normalWithPrompt,disabledWithPrompt" xmlns:e="http://ns.egret.com/eui">
-                <e:Image width="100%" height="100%" scale9Grid="1,3,8,8" source="button_up_png"/>
+                <e:Image width="100%" height="100%" scale9Grid="1,3,8,8" source="resource/button_up.png"/>
                 <e:Rect height="100%" width="100%" fillColor="0xffffff"/>
                 <e:EditableText id="textDisplay" verticalCenter="0" left="10" right="10"
                                 textColor="0x000000" textColor.disabled="0xff0000" width="100%" height="24" size="20" />
