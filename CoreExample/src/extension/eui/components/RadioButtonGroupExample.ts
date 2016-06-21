61554976f98007ef84dfdd6ebd9e618ce00c150a
zhangyu <runinspring@gmail.com>
2016-06-21 16:08:23 +0800

    update radioButtonGroup example

--- CoreExample/src/extension/eui/components/RadioButtonGroupExample.ts
+++ CoreExample/src/extension/eui/components/RadioButtonGroupExample.ts
@@ -12,20 +12,26 @@ class RadioButtonGroupExample extends egret.Sprite {
                 <e:Label x="38" id="labelDisplay" textColor="0xffffff" size="18"/>
             </e:Skin>`;
         EXML.parse(skinExml);
-
         var exml =
             `<e:Group xmlns:e="http://ns.egret.com/eui">
                 <e:layout>
                     <e:VerticalLayout gap="20"/>
                 </e:layout>
-                <e:RadioButton groupName="en" label="A. This is a radioButton." skinName="skins.RadioButtonSkin"/>
-                <e:RadioButton groupName="en" label="B. This is a progressBar." skinName="skins.RadioButtonSkin"/>
-                <e:RadioButton groupName="cn" label="1. 今天是晴天." skinName="skins.RadioButtonSkin"/>
-                <e:RadioButton groupName="cn" label="2. 今天是雨天." skinName="skins.RadioButtonSkin"/>
+
+                <e:RadioButton id="radio1" groupName="en" label="A. This is a radioButton." skinName="skins.RadioButtonSkin"/>
+                <e:RadioButton id="radio2" groupName="en" label="B. This is a progressBar." skinName="skins.RadioButtonSkin"/>
+                
+                <e:RadioButton id="radio3" groupName="cn" label="1. 今天是晴天." skinName="skins.RadioButtonSkin"/>
+                <e:RadioButton id="radio4" groupName="cn" label="2. 今天是雨天." skinName="skins.RadioButtonSkin"/>
             </e:Group>`;
         var clazz = EXML.parse(exml);
         var group = new clazz();
         this.addChild(group);
+        group.radio1.group.addEventListener(eui.UIEvent.CHANGE, (evt) => { 
+            var radioGroup: eui.RadioButtonGroup = evt.target;
+            console.log(radioGroup.selectedValue);
+            console.log(group.radio1.selected);
+        },this)
         group.x = 50;
         group.y = 50;
     }
