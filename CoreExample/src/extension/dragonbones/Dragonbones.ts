2f03d9caf161cfca80eb8fd05bf5242790fa3767
himuil <jerry.lin@egret.com>
2017-09-18 14:00:19 +0800

    Modify dragonbones

--- CoreExample/src/extension/dragonbones/Dragonbones.ts
+++ CoreExample/src/extension/dragonbones/Dragonbones.ts
@@ -25,22 +25,16 @@ class DragonbonesExample extends egret.DisplayObjectContainer {
         this.createExample();
     }
     public createExample():void {
-        var container = new egret.DisplayObjectContainer();
+        const factory = dragonBones.EgretFactory.factory;
+        factory.parseDragonBonesData(this.skeletonData);
+        factory.parseTextureAtlasData(this.textureData, this.texture);
 
-        egret.MainContext.instance.stage.addChild(container);
-        container.x = 50;
+        const armatureDisplay = factory.buildArmatureDisplay("DragonBoy");
+        armatureDisplay.animation.play("walk");
 
-        var factory = new dragonBones.EgretFactory();
-        factory.addSkeletonData(dragonBones.DataParser.parseDragonBonesData(this.skeletonData));
-        factory.addTextureAtlas(new dragonBones.EgretTextureAtlas(this.texture, this.textureData));
-
-        var armature = factory.buildArmature("Dragon");
-        var armatureDisplay = armature.getDisplay();
-        dragonBones.WorldClock.clock.add(armature);
-        container.addChild(armatureDisplay);
-        armatureDisplay.x = 200;
-        armatureDisplay.y = 450;
-        armature.animation.gotoAndPlay("walk");
+        armatureDisplay.x = this.stage.stageWidth * 0.5;
+        armatureDisplay.y = this.stage.stageHeight * 0.5 + 100;
+        this.addChild(armatureDisplay);
 
         egret.Ticker.getInstance().register(function (advancedTime) {
             dragonBones.WorldClock.clock.advanceTime(advancedTime / 1000);
